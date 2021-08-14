f5e574fabfaa7ab8eba40656aa24e20ff7be0dec
gogowonji <65698313+gogowonji@users.noreply.github.com>
2021-08-14 18:26:03 +0900

    first commit

---
+++ youtube-onedaybuild/youtube_onedaybuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  youtube_onedaybuildApp.swift
+//  youtube-onedaybuild
+//
+//  Created by 지원 on 2021/08/14.
+//
+
+import SwiftUI
+
+@main
+struct youtube_onedaybuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
